dac1a01b8a2d0f012c3ec313a287bfda1560ac2c
Pavel Andreyev <paaha98@gmail.com>
2018-10-05 14:01:59 +0300

    Fields created

--- src/Rows.java
+++ src/Rows.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rows {
+
+    private List<DataRow> dataBase;
+
+    public Rows() {
+        dataBase = new ArrayList<>();
+    }
+
+    public DataRow getItem(int index){
+        return dataBase.get(index);
+    }
 }
