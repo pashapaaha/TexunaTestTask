14038b8d1cb1cb6a54f5943e9f6140fcfda9ff59
Pavel Andreyev <paaha98@gmail.com>
2018-10-06 16:44:36 +0300

    move (2/2)

--- src/StaxStreamProcessor.java
+++
@@ -1,8 +0,0 @@
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.events.XMLEvent;
-import java.io.InputStream;
-
-public class StaxStreamProcessor {
-}
