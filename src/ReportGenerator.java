93b2534e527e19dc60ad7c463f7f27be5a9ad3cb
Pavel Andreyev <paaha98@gmail.com>
2018-10-06 18:38:00 +0300

    Getting data from tsv file

--- src/ReportGenerator.java
+++ src/ReportGenerator.java
@@ -1,6 +1,6 @@
 public class ReportGenerator {
 
     public static void main(String[] args) {
-
+        Rows rows = new Rows();
     }
 }
--- src/Rows.java
+++ src/Rows.java
@@ -1,3 +1,8 @@
+import com.univocity.parsers.tsv.TsvParser;
+import com.univocity.parsers.tsv.TsvParserSettings;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,8 +10,24 @@ public class Rows {
 
     private List<DataRow> dataBase;
 
-    public Rows() {
+    Rows() {
         dataBase = new ArrayList<>();
+        TsvParserSettings settings = new TsvParserSettings();
+        settings.getFormat().setLineSeparator("\n");
+
+        TsvParser parser = new TsvParser(settings);
+
+        try {
+            List<String[]> allRows = parser.parseAll(new FileInputStream("source-data.tsv"));
+            for(String[] arr : allRows){
+                for(String str: arr){
+                    System.out.print(str+", ");
+                }
+                System.out.println();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public DataRow getItem(int index){
