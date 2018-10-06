5c2b67b3e0aab4dd64bf91962b84188c30653887
Pavel Andreyev <paaha98@gmail.com>
2018-10-06 18:05:47 +0300

    Getting struct from xml file

--- src/ReportGenerator.java
+++ src/ReportGenerator.java
@@ -1,6 +1,7 @@
 public class ReportGenerator {
 
     public static void main(String[] args) {
-
+        String xmlFileName = "settings.xml";
+        BaseStruct bs = new BaseStruct(xmlFileName);
     }
 }
