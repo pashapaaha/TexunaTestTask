57060c3754edd3c00147c8992b8856cde66867c9
Pavel Andreyev <paaha98@gmail.com>
2018-10-06 16:38:33 +0300

    feature to another branch (1/2)

--- src/StaxStreamProcessor.java
+++ src/StaxStreamProcessor.java
@@ -1,50 +1,8 @@
+import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.events.XMLEvent;
 import java.io.InputStream;
 
-public class StaxStreamProcessor implements AutoCloseable {
-
-    private final XMLStreamReader reader;
-
-    public StaxStreamProcessor(InputStream is) throws XMLStreamException {
-        reader = FACTORY.createXMLStreamReader(is);
-    }
-
-    public XMLStreamReader getReader() {
-        return reader;
-    }
-
-    public boolean startElement(String element, String parent) throws XMLStreamException {
-        while (reader.hasNext()) {
-            int event = reader.next();
-            if (parent != null && event == XMLEvent.END_ELEMENT &&
-                    parent.equals(reader.getLocalName())) {
-                return false;
-            }
-            if (event == XMLEvent.START_ELEMENT &&
-                    element.equals(reader.getLocalName())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public String getAttribute(String name) throws XMLStreamException {
-        return reader.getAttributeValue(null, name);
-    }
-
-    public String getText() throws XMLStreamException {
-        return reader.getElementText();
-    }
-
-    @Override
-    public void close() throws Exception {
-        if (reader != null) {
-            try {
-                reader.close();
-            } catch (XMLStreamException e) { // empty
-            }
-        }
-    }
+public class StaxStreamProcessor {
 }
